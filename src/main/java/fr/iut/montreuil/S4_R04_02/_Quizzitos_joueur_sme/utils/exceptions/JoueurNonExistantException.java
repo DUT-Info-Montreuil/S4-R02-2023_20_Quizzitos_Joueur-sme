5fc115f643f6cc857e3d09e04a4da9b495e3a8a8
Nicolas Ramirez <ramirez.nicolas2305@hotmail.com>
2023-05-22 10:03:32 +0200

    deplacement du serviceJoueurBean, ajout des declaration dans la signature IserviceJoueur, configuration des import, mal interpreté par le pc de l'iut

--- src/main/java/fr/iut/montreuil/S4_R04_02/_Quizzitos_joueur_sme/utils/exceptions/JoueurNonExistantException.java
+++ src/main/java/fr/iut/montreuil/S4_R04_02/_Quizzitos_joueur_sme/utils/exceptions/JoueurNonExistantException.java
@@ -1,4 +1,4 @@
-package src.main.java.fr.iut.montreuil.S4_R04_02._Quizzitos_joueur_sme.utils.exceptions;
+package fr.iut.montreuil.S4_R04_02._Quizzitos_joueur_sme.utils.exceptions;
 
 public class JoueurNonExistantException extends Exception{
 
--- src/main/java/fr/iut/montreuil/S4_R04_02/_Quizzitos_joueur_sme/utils/exceptions/parametreManquantException.java
+++
@@ -1,5 +0,0 @@
-package src.main.java.fr.iut.montreuil.S4_R04_02._Quizzitos_joueur_sme.utils.exceptions;
-
-public class parametreManquantException extends Exception{
-
-}
